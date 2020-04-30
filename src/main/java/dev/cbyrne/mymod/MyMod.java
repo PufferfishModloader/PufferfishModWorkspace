0dd301a7f48968091b815342d9cb9a5ef61dfe29
Conor Byrne <con0r.b@protonmail.com>
2020-04-30 13:36:28 +0100

    Initial Commit

---
+++ src/main/java/dev/cbyrne/mymod/MyMod.java
@@ -0,0 +1,14 @@
+package dev.cbyrne.mymod;
+
+import dev.cbyrne.pufferfishmodloader.mods.core.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "mymod")
+public class MyMod {
+    private static final Logger LOGGER = LogManager.getLogger("MyMod");
+
+    public void start() {
+        LOGGER.info("MyMod started!");
+    }
+}
